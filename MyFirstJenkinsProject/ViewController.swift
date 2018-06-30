76029a866d96daf7ccaf11d71bb2097ab9a4ff9f
Jenkins <jenkins@Pratibhas-MacBook-Pro.local>
2018-07-01 01:05:44 +0530

    Initial commit

---
+++ MyFirstJenkinsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstJenkinsProject
+//
+//  Created by Jenkins on 01/07/18.
+//  Copyright © 2018 Jenkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
